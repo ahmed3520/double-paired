e98e4edf415273a8ad4affafae1fcf3f99a403ee
ahmed3520 <ataha352000@gmail.com>
2021-12-11 00:33:46 +0200

    finish the double paired

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
 file1 = open('sq.txt', 'r')
+firstLine = file1.readline()
+file_test = open('ReadPairsOutput.txt', 'r')
+file_test_check = file_test.readline()
 Lines = file1.readlines()
+
+
+def findSum(str1):
+    temp = "0"
+
+    Sum = 0
+
+    for ch in str1:
+
+        if (ch.isdigit()):
+            temp += ch
+
+        else:
+
+            Sum += int(temp)
+
+            temp = "0"
+
+    return Sum + int(temp)
+
+
+kAndD = 0
+kAndD += findSum(firstLine)
 d = {}
 path = []
 count = 0
-# Strips the newline character
+prefix_count = 0
+
 L = ""
-start = 4
-stop = 6
+start = 30
+stop = 100
+prefixString = ""
+suffexString = ""
 
 for line in Lines:
-    Line = line[0: start:] + line[stop + 1::]
-    d[(Line[0:len(Line) - 6], Line[4:len(Line) - 2])] = (Line[1:len(Line) - 5], Line[5:len(Line) - 1])
-print("D", d)
+    Line = line[:30] + line[30 + 1:]
+    # print("LINE=>", len(Line))
+    print(line)
+    print(Line[31: len(Line) - 1])
+    d[(Line[0:len(Line) - 32], Line[30:len(Line) - 2])] = (Line[1:len(Line) - 31], Line[31:len(Line) - 1])
+    # print(d)
 
 
 def check_start_point(key_check):
@@ -31,21 +63,58 @@ def check_modulus():
 
 def check_path(value):
     for key_index in d:
-        print(value, "key index :-", key_index)
         if value == key_index:
-            print("kkkk")
             path.append(key_index)
             path.append(d[key_index])
 
+
+def get_prefix(pathLen, key_p, counter):
+    prefix = ""
+
+    if counter == pathLen - 1:
+        prefix += key_p
+    else:
+        prefix += key_p[0]
+    counter += 1
+    return prefix
+
+
+def get_suffex(pathLen, second_val, counter):
+    suffex = ""
+
+    if counter == pathLen - 1:
+        suffex += second_val
+    else:
+        suffex += second_val[0]
+    counter += 1
+    return suffex
+
+
 for key in d:
-    print(key, ":", d[key])
-    print(path)
     if not check_start_point(key):
         path.append(key)
         path.append(d[key])
-        print(path)
         check_modulus()
         path = list(dict.fromkeys(path))
-        print(path)
+        count = 0
+        for key_p, c in path:
+            prefixString += get_prefix(len(path), key_p, count)
+            suffexString += get_suffex(len(path), c, count)
+            count += 1
         break
+sim = ""
 
+
+def get_KD(sim):
+    sim += prefixString
+    sim += suffexString[-kAndD:]
+    # print()
+    #new_counter = 0
+    #for i in reversed(suffexString):
+        #if new_counter < kAndD:
+            #sim += i
+            #new_counter += 1
+    print("sooo=>", sim)
+
+
+get_KD(sim)
